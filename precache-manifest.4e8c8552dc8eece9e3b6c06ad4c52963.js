74a0398f08614a97dbba03a1e04f17d4b57714b4
kyogan12 <kyoyoyo122@gmail.com>
2022-06-21 09:58:15 +0200

    Updates

---
+++ precache-manifest.4e8c8552dc8eece9e3b6c06ad4c52963.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "25d8ded855accc112794",
+    "url": "/Chainsaw-man-memory-game/static/css/main.3b1e2c93.chunk.css"
+  },
+  {
+    "revision": "25d8ded855accc112794",
+    "url": "/Chainsaw-man-memory-game/static/js/main.f0ac606c.chunk.js"
+  },
+  {
+    "revision": "73aa3bd4d3a4584d81d7",
+    "url": "/Chainsaw-man-memory-game/static/js/runtime~main.af8d689d.js"
+  },
+  {
+    "revision": "91568b34f8e8624958a2",
+    "url": "/Chainsaw-man-memory-game/static/js/2.ae733d23.chunk.js"
+  },
+  {
+    "revision": "7b10aa2ccaf2348f6ebcfb54a766d504",
+    "url": "/Chainsaw-man-memory-game/static/media/Aki_old.7b10aa2c.webp"
+  },
+  {
+    "revision": "135c88d39a8038542d742c41514d916a",
+    "url": "/Chainsaw-man-memory-game/static/media/Angel_Devil.135c88d3.webp"
+  },
+  {
+    "revision": "cf5bf22bdfcf0b42535e70949bb90b32",
+    "url": "/Chainsaw-man-memory-game/static/media/EyepatchDenji.cf5bf22b.webp"
+  },
+  {
+    "revision": "7b0db448ec3c50dc3404d4695d38af88",
+    "url": "/Chainsaw-man-memory-game/static/media/Gun_devil.7b0db448.webp"
+  },
+  {
+    "revision": "db992491dfd7a4cd673002c8a456bffc",
+    "url": "/Chainsaw-man-memory-game/static/media/Kishibe.db992491.webp"
+  },
+  {
+    "revision": "4d12c0dbb34fba4b679ed587f0aa1ea2",
+    "url": "/Chainsaw-man-memory-game/static/media/Power.4d12c0db.webp"
+  },
+  {
+    "revision": "85955429e24e64e77cab396936d5c82e",
+    "url": "/Chainsaw-man-memory-game/index.html"
+  }
+];
